22faaa9f6391de0b0abb1ba10ce1537dcbbd43d0
Lucille Delisle <lucille.delisle@unige.ch>
2025-04-02 15:08:25 +0200

    fix fromMacs2BdgToSimplifiedBdgAndBw.sh to make it work with any nb of chromosomes

--- hpc/helper_scripts/fromMacs2BdgToSimplifiedBdgAndBw.sh
+++ hpc/helper_scripts/fromMacs2BdgToSimplifiedBdgAndBw.sh
@@ -4,30 +4,27 @@ outputBasename=$2 # The outputBasename of both bedgraph and bw, for example ${pa
 nameOfTheTrack=$3 # For example "macs2 likeATAC of $sample"
 pathForChromSizes=$4 # For example $pathForFasta/${genome}.fa.fai
 
-# First put the chromosome sizes in a string
-chromSizes=`cat "$pathForChromSizes" | tr "\t" ":" | tr "\n" ","`
 # There are 2 issues with converting bedgraph from macs2 to bw:
 # 1) The chromosome names are not sorted
 # 2) Sometimes the coverage goes over the size of the chromosome
 # Quicker than bedtools sort is to output each chromosome in a different file and then do a cat.
 myTmp=`mktemp`
-cat "$inputBedGraph" | awk -v h="${nameOfTheTrack}" -v tmp=$myTmp -v s=$chromSizes 'BEGIN{
+awk -v h="${nameOfTheTrack}" -v tmp=$myTmp 'BEGIN{
   print "track type=bedGraph name=\""h"\" visibility=full autoScale=on alwaysZero=on windowingFunction=mean"
   OFS="\t"
-  split(s,mychrs,",");
-  for (i in mychrs){
-    split(mychrs[i],nameAndSize,":");
-    chromSizes[nameAndSize[1]] = nameAndSize[2]
-  }
 }
-$4!=0 && $1 != "track"{
+# Deal with chromosome size
+NR == FNR{
+    chromSizes[$1] = $2
+}
+NR != FNR && $4!=0 && $1 != "track"{
   if ($2<chromSizes[$1]){
     if($3>chromSizes[$1]){
       $3=chromSizes[$1]
     }
     print $0 > tmp"_"$1
   }
-}' > "${outputBasename}.bedGraph"
+}' "$pathForChromSizes" "$inputBedGraph" > "${outputBasename}.bedGraph"
 cat ${myTmp}_* >> "${outputBasename}.bedGraph"
 bedGraphToBigWig "${outputBasename}.bedGraph" "${pathForChromSizes}" "${outputBasename}.bw"
 if [ -e "${outputBasename}.bedGraph.gz" ]; then
